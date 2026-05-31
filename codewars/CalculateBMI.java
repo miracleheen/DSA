8747dd7f3ca290e06526ba2c2ec927461c81655b
Miracle- <abed90eg@gmail.com>
2026-05-31 17:56:35 +0300

    Calculate BMI (fundamental)

---
+++ codewars/CalculateBMI.java
@@ -0,0 +1,6 @@
+public class CalculateBMI {
+  public static String bmi(double weight, double height) {
+      double bmi = weight / (height * height);
+      return bmi <= 18.5 ? "Underweight" : bmi <= 25.0 ? "Normal" : bmi <= 30.0 ? "Overweight" : "Obese";
+  }
+}
