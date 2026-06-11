4c578d4be3da71bb03de6872ca4ea0bb3e3290cf
Miracle- <abed90eg@gmail.com>
2026-06-11 22:40:05 +0300

    Tribonacci Sequence

---
+++ codewars/Xbonacci.java
@@ -0,0 +1,12 @@
+public class Xbonacci {
+  public double[] tribonacci(double[] s, int n) {
+    double[] result = new double[n];
+
+    for (int i = 0; i < n; i++) {
+      if (i < 3) result[i] = s[i];
+      else result[i] = result[i - 1] + result[i - 2] + result[i - 3];
+    }
+
+    return result;
+  }
+}
